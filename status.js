3516b90638b28ed85efc9ab9a8946a1e028831f1
autecht <autecht@ucsd.edu>
2022-12-23 22:39:04 -0800

    add images

--- status.js
+++ status.js
@@ -1,3 +1,19 @@
+const pageList = [
+    "index.html",
+    "1.html",
+    "2.html",
+    "3.html",
+    "4.html",
+    "5.html",
+    "6.html",
+    "7.html",
+    "8.html",
+    "9.html",
+    "10.html",
+    "11.html",
+    "12.html",
+];
+
 function cycleLeft() {
     alert("Left");
 }
