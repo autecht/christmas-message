e89772309b929c2e7eabec5a7568af3da6782900
autecht <autecht@ucsd.edu>
2022-12-25 16:22:21 -0800

    add cycling feature

--- status.js
+++ status.js
@@ -12,11 +12,33 @@ const imageList = [
     "image11.jpg",
     "image12.jpg",
 ];
+/*
+ * represents what page/image user is on
+ * 0 represents homepage, 1-12 represents image
+ */
+var currentStatus = 0; // homepage
 
 function cycleLeft() {
-    alert("Left");
+    currentStatus -= 1;
+    currentStatus %= 12;
+    changePage(currentStatus);
 }
 
 function cycleRight() {
-    alert("Right");
+    currentStatus += 1;
+    currentStatus %= 12;
+    changePage(currentStatus);
+}
+
+function changePage(currentStatus) {
+    if (currentStatus == 0) {
+        window.location.assign("https://autecht.github.io/index.html");
+        return;
+    }
+
+    if ((currentStatus == 1) || (currentStatus == 12)) {
+        window.location.assign("https://autecht.github.io/christmas-message/images.html"); 
+    }
+    const imageUrl = "images/image" + (currentStatus - 1);
+    document.getElementById("image-body").style.backgroundImage = imageUrl;
 }
